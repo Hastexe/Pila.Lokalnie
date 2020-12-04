033b407a7e01c26f217e31ea9e801c85952b7176
Hastexe <michalek.radoslaw@gmail.com>
2020-12-04 14:28:37 +0100

    Dodawanie ogloszen
    poprawione - git zepsuł poprzednie wysłanie

---
+++ MajsterFinale/Models/AdvertRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MajsterFinale.Models
+{
+    public class AdvertRespository
+    {
+        private BazaLocal db = new BazaLocal();
+        public ADVERTS GetAdverts(int id)
+        {
+            return db.ADVERTS.SingleOrDefault(d => d.ID == id);
+        }
+    }
+}
